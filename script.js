5e64b8a16504b4bbaa39e8918ee3e05db63bf389
Abushka2005 <legionislegend@gmail.com.noreply.github.com>
2022-07-10 19:16:40 +0500

    Testirovkadan otmoqda

---
+++ script.js
@@ -0,0 +1,67 @@
+// let elDiv = document.querySelector('.f1-span')
+// const para = document.createElement("button");
+// para.innerText = "Menu";
+// elDiv.appendChild(para);
+// para.classList.add('myBtn');
+
+
+// function tr(){
+//     if(el){
+//          const para = document.createElement("button");
+//          para.innerText = "Menu";
+//          elDiv.appendChild(para);
+//          para.classList.add('myBtn');
+//     }
+//     else{
+//         console.log('Not yet');
+//     }
+    
+// }
+// tr()
+
+
+
+
+
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
+  let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+    window.addEventListener('load', function () {
+        spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+    });
+
+
+
+
+
+
+
+
+
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
